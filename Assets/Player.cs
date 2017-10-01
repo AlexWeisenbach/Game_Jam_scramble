24ffac5e32bb38976980b6102d8621538f3696a6
Alex w <weisea3@rpi.edu>
2017-10-01 02:02:48 -0400

    walk animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,10 +10,15 @@ public class Player : MonoBehaviour {
     bool runMode = true;
     bool canJump = true;
 
-    public float scale = 0.5f;
+    Animator anim;
+
+    public float scale = 1.5f;
+    public float runScale = 1.5f;
+    public int direction;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -23,12 +28,30 @@ public class Player : MonoBehaviour {
             if (Input.GetKey("right"))
             {
                 rb.velocity = new Vector3(playerSpeed, rb.velocity.y, 0);
+                //transform.localScale = new Vector3(runScale, runScale, 1);
                 transform.localScale = new Vector3(scale, scale, 1);
+                direction = 0;
+                anim.SetBool("walking", true);
             }
             else if(Input.GetKey("left"))
             {
                 rb.velocity = new Vector3(-1*playerSpeed, rb.velocity.y, 0);
+                //transform.localScale = new Vector3(-1 * runScale, runScale, 1);
                 transform.localScale = new Vector3(-1 * scale, scale, 1);
+                direction = 1;
+                anim.SetBool("walking", true);
+            }
+            if(!Input.anyKey)
+            {
+                if (direction == 0)
+                {
+                    transform.localScale = new Vector3(scale, scale, 1);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(-1* scale, scale, 1);
+                }
+                anim.SetBool("walking", false);
             }
             if(Input.GetKeyDown("space") && canJump)
             {
@@ -42,6 +65,7 @@ public class Player : MonoBehaviour {
     {
         runMode = false;
         rb.simulated = false;
+        anim.SerBool("walking", false);
     }
 
     void ClimbingWallEnd()
